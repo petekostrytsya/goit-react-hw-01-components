6ae39495a5f7ce45e179da8b8cbf2b6c8cfb73db
petekostrytsya <petekostrytsya@gmail.com>
2023-05-18 23:17:29 +0300

    add#1#1finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,8 @@
+import React from 'react';
 import { Profile } from 'components/Profile/Profile';
+import user from 'user.json';
+
+
 
 export const App = () => {
   return (
@@ -11,8 +15,15 @@ export const App = () => {
         fontSize: 40,
         color: '#010101'
       }}
-    >
-      React homework template
+    > <div>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </div>
     </div>
-  );
-};
+  )
+}
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,43 +1,46 @@
 import PropTypes from "prop-types";
 
-export const Profile = ({ username, tag, location, avatar, stats }) => {
-  return (  <div class="profile">
-  <div class="description">
+
+export const Profile = ({ username, tag, location, avatar, stats: {followers, views, likes} }) => {
+  return (  <div className ="profile">
+  <div className ="description">
     <img
-      src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+      src={avatar}
       alt="User avatar"
       class="avatar"
     />
-    <p class="name">Petra Marica</p>
-    <p class="tag">@pmarica</p>
-    <p class="location">Salvador, Brasil</p>
+    <p className ="name">{username}</p>
+    <p className ="tag">@{tag}</p>
+    <p className ="location">{location}</p>
   </div>
 
-  <ul class="stats">
+  <ul className ="stats">
     <li>
-      <span class="label">Followers</span>
-      <span class="quantity">1000</span>
+        <span className="label">{followers}</span>
+      <span className ="quantity">1000</span>
     </li>
     <li>
-      <span class="label">Views</span>
-      <span class="quantity">2000</span>
+      <span className ="label">{views}</span>
+      <span className ="quantity">2000</span>
     </li>
     <li>
-      <span class="label">Likes</span>
-      <span class="quantity">3000</span>
+      <span className ="label">{likes}</span>
+      <span className ="quantity">3000</span>
     </li>
   </ul>
 </div>
     )
 }
 
-Profile.PropTypes = {
+Profile.propTypes = {
     username: PropTypes.string.isRequired,
     tag: PropTypes.string.isRequired,
     location: PropTypes.string.isRequired,
     avatar: PropTypes.string.isRequired,
-    stats: PropTypes.objectOf({
+  stats: PropTypes.objectOf(
+    PropTypes.exact ({
     label: PropTypes.string.isRequired,
     quantity: PropTypes.number.isRequired,
-  }),
+  }),)
 }
+
